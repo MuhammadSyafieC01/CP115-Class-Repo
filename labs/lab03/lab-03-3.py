acc03693cc6f9a650567a0de5fb11a7144458178
MuhammadSyafieC01 <monstared02@gmail.com>
2025-07-29 11:36:49 +0800

    commit

--- labs/lab03/lab-03-3.py
+++ labs/lab03/lab-03-3.py
@@ -3,3 +3,7 @@ megabyte = kilobyte / 1000
 gigabyte = kilobyte / 1000 / 1000
 terabyte = kilobyte / 1000 / 1000 / 1000
 petabyte = kilobyte / 1000 / 1000 / 1000 / 1000
+print(megabyte)
+print(gigabyte)
+print(terabyte)
+print(petabyte)
