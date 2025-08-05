1e155ac2e6589b2edb2e7228809ce777b1301afd
MuhammadSyafieC01 <monstared02@gmail.com>
2025-08-05 12:51:29 +0800

    army

--- labs/lab05/test.py
+++ labs/lab05/test.py
@@ -4,3 +4,17 @@ x = random.randint(1000,5000)
 y = random.randint(1000,5000)
 print(x,y)
 
+xdamage = random.randint(0,1)
+ydamage = random.randint(0,1)
+print(xdamage,ydamage)
+
+
+
+#battle
+while x or y <= 0 :
+ #x attack
+ y = y - (x * xdamage)
+
+ #y attack
+ x = x - (y * Ydamage)
+
