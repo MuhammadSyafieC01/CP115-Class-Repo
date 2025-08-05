9f81959c9637d97161ece634fb926846f26515dc
MuhammadSyafieC01 <monstared02@gmail.com>
2025-08-05 12:28:34 +0800

    pusssh

---
+++ labs/lab05/exercise2.py
@@ -0,0 +1,6 @@
+import math
+radius = float(input("Enter radius: "))
+area = 3.142 * radius * radius
+circumference = 2 * 3.142 * radius
+print("Area of circle is: ",area)
+print("Circumference of circe is: ",circumference)
