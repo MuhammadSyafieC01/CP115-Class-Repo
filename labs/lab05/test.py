18b1e41b608a4e23c2abd8cf0eff6a539a088d30
MuhammadSyafieC01 <monstared02@gmail.com>
2025-08-12 12:22:58 +0800

    test

--- labs/lab05/test.py
+++ labs/lab05/test.py
@@ -1,26 +1,11 @@
 import random
 
-x = random.randint(1000,5000)
-y = random.randint(1000,5000)
-print(x,y)
-
-xdamage = random.randint(0.1,1)
-ydamage = random.randint(0.1,1)
-print(xdamage,ydamage)
+x = 10
+y = 10
 
 
-
-#battle
-while x or y >= 0 :
- #x attack
- y = y - (x * xdamage)
-
- #y attack
- x = x - (y * ydamage)
-
-if x<=0:
- print("y win")
-
-if y<=0:
- print("x win")
-
+while y or x >= 0 :
+    print(y)
+    y=y-1
+    print(x)
+    x=x-1
