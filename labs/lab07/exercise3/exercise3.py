9ab8c67a3298d6c1c54be91d5b248dca9161624d
MuhammadSyafieC01 <monstared02@gmail.com>
2025-08-19 12:54:56 +0800

    save

---
+++ labs/lab07/exercise3/exercise3.py
@@ -0,0 +1,17 @@
+import school_data
+
+print(f"Student name: {school_data.name}")
+print(f"Student ID: {school_data.student_id}")
+print(f"Age: {school_data.age}")
+
+print(f"Course code: {school_data.course_code}")
+print(f"Course name: {school_data.course_name}")
+
+print(f"First score: {school_data.score_1}")
+print(f"Second score: {school_data.score_2}")
+print(f"Total score: {school_data.total_score}")
+
+
+print(f"First score: {school_data.score_1}")
+print(f"First score: {school_data.score_1}")
+print(f"First score: {school_data.score_1}")
---
+++ labs/lab07/exercise3/school_data.py
@@ -0,0 +1,20 @@
+import random
+import math
+
+#Student information
+name = "Adam"
+student_id = 114769
+age = 18
+
+course_code = 1169
+course_name = "Computer"
+
+score_1 = random.randint(70,95)
+score_2 = random.randint(75,100)
+total_score = score_1 + score_2
+
+upper_name = name.upper()
+lower_name = name.lower()
+length_name = len(name)
+
+squar = math.sqrt(total_score)
