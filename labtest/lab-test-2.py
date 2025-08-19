17f604ccbba738e3a100be01cb3edd0cc377b442
MuhammadSyafieC01 <monstared02@gmail.com>
2025-08-19 11:15:25 +0800

    testt

--- labtest/lab-test-2.py
+++ labtest/lab-test-2.py
@@ -1,5 +1,6 @@
 #Name:Muhammad Syafie Bin Ruslin
-#Problem:Writing python program using an appropriate escape echaracters and arithmetic expression
+#Problem:Writing python program using an appropriate escape characters and arithmetic expression
+
 mark=10
 full_mark=mark*2
 print("Name: Ahmad Bin Abu\t\t\tMatric. No: MS2025123499\n*\t\t *\n**\t    **\n***    ***\n********\n***    ***\n**\t    **\n*\t\t *\nThis is my\n\tsecond\n\t      assignment\nI want 2x10 marks,which is",full_mark,"full marks")
