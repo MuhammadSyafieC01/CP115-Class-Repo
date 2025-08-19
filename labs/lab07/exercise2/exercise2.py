4071bf4609feebefbd4c4c7af83b97ccb4ffa32d
MuhammadSyafieC01 <monstared02@gmail.com>
2025-08-19 12:31:51 +0800

    pusssshhhhh

---
+++ labs/lab07/exercise2/exercise2.py
@@ -0,0 +1,7 @@
+import shopping_data
+print(f"Product name: {shopping_data.product}")
+print(f"Price: {shopping_data.price}")
+print(f"Quantity: {shopping_data.quantity}")
+print(f"Total cost: {shopping_data.total_cost}")
+print(f"Product in UPPERCASE: {shopping_data.upper_product}")
+print(f"Product in lowercase: {shopping_data.lower_product}")
---
+++ labs/lab07/exercise2/shopping_data.py
@@ -0,0 +1,8 @@
+product = "Bread"
+price = 1
+quantity = 20
+
+total_cost = price * quantity
+
+upper_product = product.upper()
+lower_product = product.lower()
