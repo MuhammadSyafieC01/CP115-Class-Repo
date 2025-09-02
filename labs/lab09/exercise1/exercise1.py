30f64d3ebe0ac2373bcade5dec290f339ddf58cc
MuhammadSyafieC01 <monstared02@gmail.com>
2025-09-02 12:54:05 +0800

    push1

--- labs/lab09/exercise1/exercise1.py
+++ labs/lab09/exercise1/exercise1.py
@@ -5,18 +5,19 @@ gpa = float(input("Enter GPA (0.0-4.0): "))
 credit_hours = int(input("Enter credit hours: "))
 
 # TODO your code here
-if gpa >= 3.8 and credit_hours >=12:
-    dean_list = True
-if gpa >= 3.5 and credit_hours >= 12:
-    honor_role = True
+if gpa < 2.0:
+    classification = "Academic Probation"
+if credit_hours < 12:
+    classification = "Part-time students"
 if gpa >= 2.0:
-    good_standing = True
-if gpa <= 2.0:
-    probation = True
+    classification = "Good Standing"
+if gpa >= 3.5 and credit_hours >= 12:
+    classification = "Honor Roll"
+if gpa >= 3.8 and credit_hours >=12:
+    classification = "Dean's List"
 
-classification = (dean_list==True) and (honor_role==True) and (good_standing==True) and (probation==True)
 
-# Display results
+
 print(f"\nStudent: {student_name}")
 print(f"GPA: {gpa}")
 print(f"Credit Hours: {credit_hours}")
