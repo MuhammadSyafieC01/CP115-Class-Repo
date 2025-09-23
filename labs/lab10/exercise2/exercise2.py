cfbf7b2f7db34c360ffd55f09e9814dae9172e5b
MuhammadSyafieC01 <monstared02@gmail.com>
2025-09-23 12:23:40 +0800

    push

--- labs/lab10/exercise2/exercise2.py
+++ labs/lab10/exercise2/exercise2.py
@@ -2,6 +2,46 @@ age = int(input())
 accident_count = int(input())
 
 # Your code here
+if age < 25:
+    base_premium = 2400
+    if accident_count == 0:
+        penalty=0
+        total_premium = base_premium + penalty 
+    elif accident_count < 3:
+        penalty=300
+        total_premium = base_premium + penalty 
+    else:
+        penalty=600
+        total_premium = base_premium + penalty 
+elif age >= 25 and age <= 50:
+    base_premium = 1800
+    if accident_count == 0:
+        penalty=0
+        total_premium = base_premium + penalty 
+    elif accident_count < 3:
+        penalty=300
+        total_premium = base_premium + penalty 
+    else:
+        penalty=600
+        total_premium = base_premium + penalty 
+else:
+    base_premium=2000
+    if accident_count == 0:
+        penalty=0
+        total_premium = base_premium + penalty 
+    elif accident_count < 3:
+        penalty=300
+        total_premium = base_premium + penalty 
+    else:
+        penalty=600
+        total_premium = base_premium + penalty 
+
+if accident_count == 0:
+    discount_amount = 0.90
+else:
+    discount_amount = 1
+
+final_premium = total_premium * discount_amount
 
 print(base_premium)
 print(final_premium)
