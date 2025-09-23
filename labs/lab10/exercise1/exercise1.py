891145fc07c7f47bb940395ab2cd6cdb80ff62ee
MuhammadSyafieC01 <monstared02@gmail.com>
2025-09-23 11:56:50 +0800

    push

--- labs/lab10/exercise1/exercise1.py
+++ labs/lab10/exercise1/exercise1.py
@@ -3,6 +3,26 @@ overtime_hours = int(input())
 is_weekend = input()
 
 # Your code here
+hourly_rate=1.5
+
+if position == "Manager" and is_weekend == "Yes":
+    overtime_pay = overtime_hours * 35 * 1.5
+    total_pay = overtime_pay + (overtime_hours * 5)
+elif position == "Manager" and is_weekend == "No":
+    overtime_pay = overtime_hours * 35 * 1.5
+    total_pay = overtime_pay
+elif position == "Supervisor" and is_weekend == "Yes":
+    overtime_pay = overtime_hours * 25 * 1.5
+    total_pay = overtime_pay + (overtime_hours * 5)
+elif position == "Supervisor" and is_weekend == "No":
+    overtime_pay = overtime_hours * 25 * 1.5
+    total_pay = overtime_pay
+elif position == "Staff" and is_weekend == "Yes":
+    overtime_pay = overtime_hours * 18 * 1.5
+    total_pay = overtime_pay + (overtime_hours * 5)
+else:
+    overtime_pay = overtime_hours * 18 *1.5
+    total_pay = overtime_pay
 
 print(hourly_rate)
 print(overtime_pay)
